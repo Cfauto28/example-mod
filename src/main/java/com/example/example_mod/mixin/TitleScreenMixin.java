3dce1b13e2733da92fe4b341ed8dc5378750317f
Cfauto <cfautoeclasse5a@gmail.com>
2026-07-29 14:19:01 +0200

    Works

--- src/main/java/com/example/example_mod/mixin/TitleScreenMixin.java
+++ src/main/java/com/example/example_mod/mixin/TitleScreenMixin.java
@@ -5,8 +5,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.example.example_mod.ExampleMod;
-
 import net.minecraft.client.gui.screen.TitleScreen;
 
 @Mixin(TitleScreen.class)
@@ -14,6 +12,5 @@ public class TitleScreenMixin {
 
 	@Inject(method = "init", at = @At("TAIL"))
 	private void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
